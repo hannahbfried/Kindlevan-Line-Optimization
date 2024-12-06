0eee2234a996b1bf740a4eb617f97cda99f0cc40
hannahbfried <hannah.b.fried@gmail.com>
2024-12-06 15:46:25 -0500

    removing more dupes slash autosaved files

--- build_queue.m
+++
@@ -1,7 +0,0 @@
-function [wait_times] = build_queue(duration, )
-% Returns an array of wait times for all customers in our simulation
-
-T = build_arrival_distribution(duration,peak1,peak2,peak1width,peak2width,multi);
-
-% placeholder return value
-wait_times = [];
